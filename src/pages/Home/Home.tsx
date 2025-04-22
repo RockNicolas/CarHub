66af18efb03e963c843d2fce2fcd00dd7d9efcc7
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-04-22 14:05:40 -0300

    Terminando o cadastro de carro e melhorando a home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,93 +1,118 @@
-import { Container } from '../../components/container/Container'
+import { useState, useEffect } from 'react'
+import { Container } from "../../components/container/Container";
+import { Link } from 'react-router-dom'
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy
+} from 'firebase/firestore'
+import { db } from '../../services/FireBaseConnection'
 
-export function Home(){
-    return (
-      <Container>
-          <section className='bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2'>
-            <input  
-                className='w-full border-2 rounded-lg h-9 px-3 outline-none'
-                placeholder='Digite o nome do carro...'
-              />
-              <button className='bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg'>
-                Buscar
-              </button>
+interface CarsProps{
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps{
+  name: string;
+  uid: string;
+  url: string;
+}
+
+
+export function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([])
+
+  useEffect(() => {
+
+    function loadCars(){
+      const carsRef = collection(db, "cars")
+      const queryRef = query(carsRef, orderBy("created", "desc"))
+
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listcars = [] as CarsProps[];
+
+        snapshot.forEach( doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid
+          })
+        })
+
+        setCars(listcars);  
+      })
+
+    }
+
+    loadCars();
+
+  }, [])
+
+
+  return (
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome do carro..."
+        />
+        <button
+          className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+        >
+          Buscar
+        </button>
+      </section>
+
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+        Carros novos e usados em todo o Brasil
+      </h1>
+
+      <main className="grid gird-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+
+
+      {cars.map( car => (
+        <Link key={car.id} to={`/car/${car.id}`}>
+          <section className="w-full bg-white rounded-lg">
+            <img
+              className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+              src={car.images[0].url}
+              alt="Carro" 
+            />
+            <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+              <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+            </div>
+
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+            <div className="px-2 pb-2">
+              <span className="text-black">
+                {car.city}
+              </span>
+            </div>
+
           </section>
+        </Link>
+      ))}
 
-          <h1 className='font-bold text-center mt-6 text-2xl mb-4'>
-            Carros novos e usados em todo Brasil
-          </h1>
 
-          <main className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
-            <section className='w-full bg-white rounded-lg'>
-              <img 
-                className='w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all'
-                src="" 
-                alt="Carro" 
-              />
-              <p className='font-bold mt-1 mb-2 px-2'>Teste</p>
-
-              <div className='flex flex-col px-2'>
-                <span className='text-zinc-700 mb-6'>Ano 2016/2016  | 23.000 km</span>
-                <strong className='text-black font-medium text-lg'>R$ 190.000</strong>
-              </div>
-
-              <div className='w-full h-px bg-slate-200 my-2'></div>
-
-              <div className='px-2 pb-2'>
-                <span className='text-zinc-700'>
-                  Fortaleza - CE
-                </span>
-              </div>
-            </section>
-
-            <section className='w-full bg-white rounded-lg'>
-              <img 
-                className='w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all'
-                src="" 
-                alt="Carro" 
-              />
-              <p className='font-bold mt-1 mb-2 px-2'>Teste</p>
-
-              <div className='flex flex-col px-2'>
-                <span className='text-zinc-700 mb-6'>Ano 2016/2016  | 23.000 km</span>
-                <strong className='text-black font-medium text-lg'>R$ 190.000</strong>
-              </div>
-
-              <div className='w-full h-px bg-slate-200 my-2'></div>
-
-              <div className='px-2 pb-2'>
-                <span className='text-zinc-700'>
-                  Fortaleza - CE
-                </span>
-              </div>
-            </section>
-
-            <section className='w-full bg-white rounded-lg'>
-              <img 
-                className='w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all'
-                src="" 
-                alt="Carro" 
-              />
-              <p className='font-bold mt-1 mb-2 px-2'>Teste</p>
-
-              <div className='flex flex-col px-2'>
-                <span className='text-zinc-700 mb-6'>Ano 2016/2016  | 23.000 km</span>
-                <strong className='text-black font-medium text-lg'>R$ 190.000</strong>
-              </div>
-
-              <div className='w-full h-px bg-slate-200 my-2'></div>
-
-              <div className='px-2 pb-2'>
-                <span className='text-zinc-700'>
-                  Fortaleza - CE
-                </span>
-              </div>
-            </section>
-            
-          </main>
-      </Container>
-    )
-  }
-    
-  
-  
+      </main>
+    </Container>
+  )
+}
