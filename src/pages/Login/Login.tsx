3b4488f68c0e6c41fee1764dcb562b81be6d1e48
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-04-22 17:21:42 -0300

    Finalizando o projeto

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,24 +1,25 @@
 import { useEffect } from 'react'
 import logoImg from '../../assets/logo.svg'
+import { Link, useNavigate } from 'react-router-dom'
 import { Container } from '../../components/container/Container'
-import { Link , useNavigate } from 'react-router-dom'
 import { Input } from '../../components/Input/Input'
-import { useForm} from 'react-hook-form'
-import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { auth } from '../../services/FireBaseConnection'
+import toast from 'react-hot-toast'
 
 const schema = z.object({
-  email: z.string().email("Insira um email válido").nonempty("O campo email é obrigatorio"),
-  password: z.string().nonempty("O campo senha é obrigatorio")
+  email: z.string().email("Insira um email válido").nonempty("O campo email é obrigatório"),
+  password: z.string().nonempty("O campo senha é obrigatório")
 })
 
 type FormData = z.infer<typeof schema>
 
-export function Login(){
+export function Login() {
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }} = useForm<FormData>({
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
   })
@@ -27,69 +28,72 @@ export function Login(){
     async function handleLogout(){
       await signOut(auth)
     }
+
     handleLogout();
   }, [])
 
-    function onSubmit(data: FormData){
-      signInWithEmailAndPassword(auth, data.email, data.password)
-      .then(() => {
-        console.log("LOGADO COM SUCESSO")
-        navigate ("/dashboard", { replace : true })
-      })
-      .catch(err => {
-        console.log("ERRO AO LOGAR")
-        console.log(err)
-      })
-    }
 
-    return (
-      <Container>
-          <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
-              <Link to='/' className='mb-6 max-w-sm w-full'>
-                <img
-                  src={logoImg}
-                  alt='Logo do site'
-                  className='w-full'
-                />
-              </Link>
-
-              <form 
-                className='bg-white max-w-xl w-full rounded-lg p-4'
-                onSubmit={handleSubmit(onSubmit)}
-              >
-                <div className='mb-3'>
-                  <Input
-                    type="email"
-                    placeholder="Digite seu email..."
-                    name="email"
-                    error={errors.email?.message}
-                    register={register}
-                  />
-                </div>
-
-                <div className='mb-3'>
-                  <Input
-                    type="password"
-                    placeholder="Digite seu senha..."
-                    name="password"
-                    error={errors.password?.message}
-                    register={register}
-                  />
-                </div>
-
-                <button type='submit' className='bg-zinc-900 w-full rounded-md text-white h-10 font-medium'>
-                  Acessar
-                </button>
-
-              </form>
-
-               <Link to='/register'>
-                  Já possui uma conta? Cadastre-se
-              </Link>
-          </div>
-      </Container>
-    )
+  function onSubmit(data: FormData){
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then((user) => {
+      console.log("LOGADO COM SUCESSO!")
+      console.log(user)
+      toast.success("Logado com sucesso!")
+      navigate("/dashboard", { replace: true })
+    })
+    .catch(err => {
+      console.log("ERRO AO LOGAR")
+      console.log(err);
+      toast.error("Erro ao fazer o login.")
+    })
   }
-    
-  
-  
+
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img
+            src={logoImg}
+            alt="Logo do site"
+            className="w-full"
+          />
+        </Link>
+
+        <form 
+          className="bg-white max-w-xl w-full rounded-lg p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu email..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button type="submit" className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium">
+            Acessar
+          </button>
+
+        </form>
+
+        <Link to="/register">
+          Ainda não possui uma conta? Cadastre-se
+        </Link>
+
+      </div>
+    </Container>
+  )
+}
